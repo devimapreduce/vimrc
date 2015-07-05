fc9bda767f6958fad419a19fe977ecf51bb59483
ikweesung <ikweesung@gmail.com>
2015-07-06 01:52:04 +0900

    change shell script name.

---
+++ install_full_vimrc.sh
@@ -0,0 +1,15 @@
+cd ~/.vim_mine
+
+echo 'set runtimepath+=~/.vim_mine
+
+source ~/.vim_mine/vimrcs/basic.vim
+source ~/.vim_mine/vimrcs/filetypes.vim
+"source ~/.vim_mine/vimrcs/plugins_config.vim
+"source ~/.vim_mine/vimrcs/extended.vim
+
+try
+source ~/.vim_mine/my_configs.vim
+catch
+endtry' > ~/.vimrc
+
+echo "Installed the Ultimate Vim configuration successfully."
